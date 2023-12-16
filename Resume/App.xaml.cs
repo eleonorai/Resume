bd99f13881a652a3c37c0ca00e67df526f17787f
ODESI <ODESI@LAPTOP-8HJ85GUS>
2023-12-17 01:56:48 +0200

    Добавьте файлы проекта.

---
+++ Resume/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Resume
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
